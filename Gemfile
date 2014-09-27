7b074b3cc66bd0ece88f8d45d36bac243db7b15e
joe-re <joe.tialtngo@gmail.com>
2014-09-28 04:37:13 +0900

    Finish sign in

--- Gemfile
+++ Gemfile
@@ -6,17 +6,18 @@ gem 'rails', '4.1.0'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'sprockets', '2.11.0'
 gem 'bcrypt-ruby', '3.1.2'
+
 group :development, :test do
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
   gem 'rspec-rails'
   gem 'spring'
-end
-
-group :test do
   gem 'selenium-webdriver'
   gem 'capybara'
   gem 'factory_girl_rails', '4.2.1'
+  gem 'rspec-its'
+  gem 'cucumber-rails', '1.4.0', require: false
+  gem 'database_cleaner', github: 'bmabey/database_cleaner'
 end
 
 # Use SCSS for stylesheets
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe User, type: :model do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -104,5 +105,10 @@ RSpec.describe User, type: :model do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to eq false }
     end
+  end
+
+  describe 'remember token' do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
